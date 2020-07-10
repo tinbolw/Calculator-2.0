aa9967b960f78ca26dee0b42d857c99f17d6bfca
Thomas Van <tinbolw@gmail.com>
2020-07-09 22:57:40 -0700

    added all buttons

--- main.py
+++ main.py
@@ -1,9 +1,78 @@
 import random
 import math
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+def add1():
+	print("WIP")
+	#TODO
+
+def add2():
+	print("WIP")
+	#TODO
+
+def add3():
+	print("WIP")
+	#TODO
+
+def add4():
+	print("WIP")
+	#TODO
+
+def add5():
+	print("WIP")
+	#TODO
+
+def add6():
+	print("WIP")
+	#TODO
+
+def add7():
+	print("WIP")
+	#TODO
+
+def add8():
+	print("WIP")
+	#TODO
+
+def add9():
+	print("WIP")
+	#TODO
+
+def add0():
+	print("WIP")
+	#TODO
+
+root = tk.Tk()
 Label1 = Label(root, text="sample text")
 Label1.pack()
+Button1 = tk.Button(root, width = 10, height = 5, text = "1", command = add1)
+Button1.pack()
+
+Button2 = tk.Button(root, width = 10, height = 5, text = "2", command = add2)
+Button2.pack()
+
+Button3 = tk.Button(root, width = 10, height = 5, text = "3", command = add3)
+Button3.pack()
+
+Button4 = tk.Button(root, width = 10, height = 5, text = "4", command = add4)
+Button4.pack()
+
+Button5 = tk.Button(root, width = 10, height = 5, text = "5", command = add5)
+Button5.pack()
+
+Button6 = tk.Button(root, width = 10, height = 5, text = "6", command = add6)
+Button6.pack()
+
+Button7 = tk.Button(root, width = 10, height = 5, text = "7", command = add7)
+Button7.pack()
+
+Button8 = tk.Button(root, width = 10, height = 5, text = "8", command = add8)
+Button8.pack()
+
+Button9 = tk.Button(root, width = 10, height = 5, text = "9", command = add9)
+Button9.pack()
+
+Button0 = tk.Button(root, width = 10, height = 5, text = "0", command = add0)
+Button0.pack()
 
 root.mainloop()
