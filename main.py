0905b67bcb15f278f9738c4e3701b6414fe5c94c
Thomas Van <tinbolw@gmail.com>
2020-07-10 21:12:34 -0700

    redesigned whole application, now it actually looks like a calculator!

--- main.py
+++ main.py
@@ -2,85 +2,38 @@ import random
 import math
 import tkinter as tk
 
-def clearNumbers():
-	print("WIP")
-	#TODO
-
-def add1():
-	print("WIP")
-	#TODO
-
-def add2():
-	print("WIP")
-	#TODO
-
-def add3():
-	print("WIP")
-	#TODO
-
-def add4():
-	print("WIP")
-	#TODO
-
-def add5():
-	print("WIP")
-	#TODO
-
-def add6():
-	print("WIP")
-	#TODO
-
-def add7():
-	print("WIP")
-	#TODO
-
-def add8():
-	print("WIP")
-	#TODO
-
-def add9():
-	print("WIP")
-	#TODO
-
-def add0():
-	print("WIP")
-	#TODO
-
 root = tk.Tk()
 
-Label1 = tk.Label(root, text="Calculator Version 1.0.0")
-Label1.pack()
+button1Label = tk.Label(root, text = "Calculator V 1.0.0").grid(row=0, column=2)
 
-clearButton = tk.Button(root, width=5, height=2, text = "Clear", command = clearNumbers)
+outputBox = tk.Entry(root,).grid(row=1,column=2)
 
-Button1 = tk.Button(root, width = 5, height = 2, text = "1", command = add1)
-Button1.pack()
 
-Button2 = tk.Button(root, width = 5, height = 2, text = "2", command = add2)
-Button2.pack()
+button1 = tk.Button(root, width=5, height=2, text = "1")
+button1.grid(row=2, column=1)
 
-Button3 = tk.Button(root, width = 5, height = 2, text = "3", command = add3)
-Button3.pack()
+button2 = tk.Button(root, width=5, height=2, text = "2")
+button2.grid(row=2, column=2)
 
-Button4 = tk.Button(root, width = 5, height = 2, text = "4", command = add4)
-Button4.pack()
+button3 = tk.Button(root, width=5, height=2, text = "3")
+button3.grid(row=2, column=3)
 
-Button5 = tk.Button(root, width = 5, height = 2, text = "5", command = add5)
-Button5.pack()
+button4 = tk.Button(root, width=5, height=2, text = "4")
+button4.grid(row=3, column=1)
 
-Button6 = tk.Button(root, width = 5, height = 2, text = "6", command = add6)
-Button6.pack()
+button5 = tk.Button(root, width=5, height=2, text = "5")
+button5.grid(row=3, column=2)
 
-Button7 = tk.Button(root, width = 5, height = 2, text = "7", command = add7)
-Button7.pack()
+button6 = tk.Button(root, width=5, height=2, text = "6")
+button6.grid(row=3, column=3)
 
-Button8 = tk.Button(root, width = 5, height = 2, text = "8", command = add8)
-Button8.pack()
+button7 = tk.Button(root, width=5, height=2, text = "7")
+button7.grid(row=4, column=1)
 
-Button9 = tk.Button(root, width = 5, height = 2, text = "9", command = add9)
-Button9.pack()
+button8 = tk.Button(root, width=5, height=2, text = "8")
+button8.grid(row=4, column=2)
 
-Button0 = tk.Button(root, width = 5, height = 2, text = "0", command = add0)
-Button0.pack()
+button9 = tk.Button(root, width=5, height=2, text = "9")
+button9.grid(row=4, column=3)
 
 root.mainloop()
