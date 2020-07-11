8769a934c4dac0ac55111b391398fdfe185e2e25
Thomas Van <tinbolw@gmail.com>
2020-07-10 20:53:29 -0700

    added clearnumbers wip function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import random
 import math
 import tkinter as tk
 
+def clearNumbers():
+	print("WIP")
+	#TODO
+
 def add1():
 	print("WIP")
 	#TODO
@@ -43,36 +47,40 @@ def add0():
 	#TODO
 
 root = tk.Tk()
-Label1 = Label(root, text="sample text")
+
+Label1 = tk.Label(root, text="Calculator Version 1.0.0")
 Label1.pack()
-Button1 = tk.Button(root, width = 10, height = 5, text = "1", command = add1)
+
+clearButton = tk.Button(root, width=5, height=2, text = "Clear", command = clearNumbers)
+
+Button1 = tk.Button(root, width = 5, height = 2, text = "1", command = add1)
 Button1.pack()
 
-Button2 = tk.Button(root, width = 10, height = 5, text = "2", command = add2)
+Button2 = tk.Button(root, width = 5, height = 2, text = "2", command = add2)
 Button2.pack()
 
-Button3 = tk.Button(root, width = 10, height = 5, text = "3", command = add3)
+Button3 = tk.Button(root, width = 5, height = 2, text = "3", command = add3)
 Button3.pack()
 
-Button4 = tk.Button(root, width = 10, height = 5, text = "4", command = add4)
+Button4 = tk.Button(root, width = 5, height = 2, text = "4", command = add4)
 Button4.pack()
 
-Button5 = tk.Button(root, width = 10, height = 5, text = "5", command = add5)
+Button5 = tk.Button(root, width = 5, height = 2, text = "5", command = add5)
 Button5.pack()
 
-Button6 = tk.Button(root, width = 10, height = 5, text = "6", command = add6)
+Button6 = tk.Button(root, width = 5, height = 2, text = "6", command = add6)
 Button6.pack()
 
-Button7 = tk.Button(root, width = 10, height = 5, text = "7", command = add7)
+Button7 = tk.Button(root, width = 5, height = 2, text = "7", command = add7)
 Button7.pack()
 
-Button8 = tk.Button(root, width = 10, height = 5, text = "8", command = add8)
+Button8 = tk.Button(root, width = 5, height = 2, text = "8", command = add8)
 Button8.pack()
 
-Button9 = tk.Button(root, width = 10, height = 5, text = "9", command = add9)
+Button9 = tk.Button(root, width = 5, height = 2, text = "9", command = add9)
 Button9.pack()
 
-Button0 = tk.Button(root, width = 10, height = 5, text = "0", command = add0)
+Button0 = tk.Button(root, width = 5, height = 2, text = "0", command = add0)
 Button0.pack()
 
 root.mainloop()
